d3ff4cccaf3b5d09ae9b15a5e2896b834658eda7
jishan <269778513@qq.com>
2018-09-12 10:16:54 +0800

    v2

--- src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
+++ src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
@@ -21,7 +21,8 @@ public class StudentController {
 
     @RequestMapping(value = "/qryById")
     public Student qry(int id){
-        System.out.println("修改123");
+        System.out.println("this is v2");
+
         return service.queryById(id);
     }
 }
